0bbfdadca17b66903ee0aa6b8b00e626237cf6ae
Xingzhe <plainths120@outlook.com>
2026-01-04 10:28:29 +0800

    add data

--- Figures.R
+++ Figures.R
@@ -1,6 +1,30 @@
 # === GBD === ----
 # Figure 1 ----
 load("EPI.rda") # COPD data
+load("GBD_db.rda")
 load("EPI_Sugar.rda")
 load("EPI_Depression.rda")
 #
+
+
+
+
+
+
+# === NHANES === ----
+load("NH_COPD.rda") # COPD
+load("NH_DEP.rda") # depression
+load("NH_Sugar.rda") # sugar
+
+
+
+
+
+
+# === GWAS === ----
+# Sweets + Depress
+# Figure 5 ----
+## ... Figure 5A ----
+load("bbj-a-103_P6_mr_res.rda")
+load("bbj-a-103_P6_mr_dat.rda")
+
