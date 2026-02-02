463e669e3bbae37641af3423cd5a36ae7cfd286b
Xingzhe <plainths120@outlook.com>
2026-02-02 10:40:04 +0800

    Update Figures and functions

--- Figures.R
+++ Figures.R
@@ -1,3 +1,4 @@
+"R version 4.3.2"
 # === GBD === ----
 # Figure 1 ----
 load("EPI.rda") # COPD data
@@ -33,20 +34,21 @@ dev.off()
 
 # Figure 2 ----
 periods <- c("35", "45", "55", "65", "75", "85", "95")
-# ... Figure 2A ----
+# ... Figure 2A，Table S1 ----
 # frocast data
 pdf("COPD_Global_Simulate_YLDs.pdf", width = 10, height = 4)
 FSMM(EPI, GBD_db, nPredict = 20, periods = periods, Measure = "YLDs", Sex = "Both", StMM = T, adjxy = c(0, 2))
 FSMM(EPI, GBD_db, nPredict = 20, periods = periods, Measure = "YLDs", Sex = "Male", StMM = T, adjxy = c(0, 2))
 FSMM(EPI, GBD_db, nPredict = 20, periods = periods, Measure = "YLDs", Sex = "Female", StMM = T, adjxy = c(0, 2))
 dev.off()
+
 # ... Figure 2B, 2C ----
 # APC-YLDs
 pdf("COPD_APC_YLDs.pdf", width = 6, height = 4)
 res_m <- PreBAPC(EPI, GBD_db, 20, "YLDs", Region = "Global", Sex = "Male", col.fan = cm.colors, ylim = c(150, 300))
 res_f <- PreBAPC(EPI, GBD_db, 20, "YLDs", Region = "Global", Sex = "Female", col.fan = cm.colors, ylim = c(150, 300))
 dev.off()
-# Save Table S2
+# ... Table S2 ----
 rates_m <- data.frame(res_m@agestd.rate)
 rates_m$year <- as.numeric(rownames(rates_m))
 rates_m$Gender <- "Male"
@@ -113,11 +115,191 @@ dev.off()
 # === NHANES === ----
 load("NH_COPD.rda") # COPD
 load("NH_DEP.rda") # depression
-load("NH_Sugar.rda") # sugar
+load("NH_Sugar.rda") # sugar intak
+
+# merge data
+data <- merge(COPD, SUGAR, by = "SEQN", all = T)
+data <- merge(data, DEP, by = "SEQN", all = T)
+table(is.na(data[["MCQ160O"]]))
+data <- data[!is.na(data[["MCQ160O"]]), ]
+#
+subDat <- data[, c(
+    "SEQN", "RIAGENDR", "RIDAGEYR", "MCQ160O", "DR1ISUGR",
+    "DPQ020", "RIDRETH1", "DMDEDUC2", "DMDMARTL", "SIAPROXY",
+    "DMDFMSIZ", "DMDHHSZA", "DMDHHSZB", "DMDHHSZE"
+)]
+colnames(subDat) <- c(
+    "SEQN", "Gender", "Age", "COPD", "Sugar",
+    "Depressed", "Race", "Education", "Marital", "Proxy",
+    "Total", "TotalChild", "TotalYouth", "TotalOld"
+)
+
+# Table 1 ----
+vars <- c(
+    "Age", "Gender", "Race", "Education", "Marital",
+    "Proxy", "Sugar", "Depressed", "Total", "TotalChild", 
+    "TotalYouth", "TotalOld"
+)
+tab_unadjust <- tableone::CreateTableOne(
+    vars = vars,
+    strata = "COPD",
+    data = dsubDat,
+    test = T, smd = F, 
+    addOverall = TRUE
+)
+
+# unadjusted SMD
+baseLine = print(tab_unadjust, showAllLevels = TRUE, smd = TRUE, nonnormal = "age")
+write.csv(baseLine, "baseLine_RAW.csv")
+
+# Table S3 ----
+library(MatchIt)
+model <- as.formula(paste0('COPD', "~", paste0(c('Gender', 'Age'), collapse = "+")))
+f <- matchit(model, dsubDat, method = "nearest", caliper = 0.1, verbose = T)
+mdata <- match.data(f)
+#
+tab_adjust <- tableone::CreateTableOne(
+    vars = vars,
+    strata = "COPD",
+    data = mdata,
+    test = T, smd = F, 
+    addOverall = TRUE
+)
+# 
+baseLine = print(tab_adjust, showAllLevels = TRUE, smd = TRUE, nonnormal = "age")
+write.csv(baseLine, "baseLine_PSM.csv")
+
+# Figure 3 ----
+## ... Figure 3A ----
+# Forestplot
+subDat$COPD <- as.numeric(subDat$COPD) - 1
+data("MORT", package = "BBX")
+head(MORT)
+data2 <- merge(subDat, MORT, by.x = "SEQN", by.y = "seqn")
+#
+pdf("Forestplot_Quantiles.pdf", width = 8, height = 5)
+ForestFit(subDat, "COPD", "Gender", subgroups = c("Sugar", "Depressed"))
+ForestFit(data2, "mortstat", "Gender", "permth_int", subgroups = c("Sugar", "Depressed"))
+dev.off()
+
+unique(subDat$COPD)
+# subDat = subDat[!is.na(subDat$Sugar), ]
+# subDat$Sugar = as.numeric(as.character(subDat$Sugar))
+subDat$COPD <- as.numeric(as.character(subDat$COPD))
+## ... Figure 3B ----
+# model 1, without covariates
+# left part
+Res <- ForestFitXX(subDat, "COPD", c("Sugar", "Depressed"),
+    PSM = c("Gender", "Age"),
+    fullPlot = F, footnote = "Match gender & age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_PSM_C.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
+
+# add time
+data("MORT", package = "BBX")
+head(MORT)
+data2 <- merge(subDat, MORT, by.x = "SEQN", by.y = "seqn")
+# right part
+Res <- ForestFitXX(data2, "COPD", c("Sugar", "Depressed"), "permth_int",
+    PSM = c("Gender", "Age"),
+    fullPlot = F, footnote = "Match gender & age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_PSM_C_T.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
+
+## ... Figure 3C ----
+# model 2, with covariates: gender, age
+# left part
+Res <- ForestFitXX(subDat, "COPD", c("Sugar", "Depressed"),
+    Covs = c("Gender", "Age"), PSM = c("Gender", "Age"),
+    fullPlot = F, footnote = "Adjust/Match gender & age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
+
+# right part, add time
+Res <- ForestFitXX(data2, "COPD", c("Sugar", "Depressed"), "permth_int",
+    Covs = c("Gender", "Age"), PSM = c("Gender", "Age"),
+    fullPlot = F, footnote = "Adjust/Match gender & age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C_T.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
 
 
+# Figure 4 ----
+## ... Figure 4A ----
+# Male
+head(subDat)
+subDat_M <- subDat[subDat$Gender == "Male", ]
+# model 2, with covariates: age
+Res <- ForestFitXX(subDat_M, "COPD", c("Sugar", "Depressed"),
+    Covs = c("Age"), PSM = c("Age"),
+    fullPlot = F, footnote = "Male. Adjust/Match age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C_Male.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
 
+#
+data2_M <- data2[data2$Gender == "Male", ]
+Res <- ForestFitXX(data2_M, "COPD", c("Sugar", "Depressed"), "permth_int",
+    Covs = c("Age"), PSM = c("Age"),
+    fullPlot = F, footnote = "Male. Adjust/Match age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C_T_Male.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
 
+## ... Figure 4B ----
+# Female
+subDat_F <- subDat[subDat$Gender == "Female", ]
+# model 2, with covariates: age
+Res <- ForestFitXX(subDat_F, "COPD", c("Sugar", "Depressed"),
+    Covs = c("Age"), PSM = c("Age"),
+    fullPlot = F, footnote = "Female. Adjust/Match age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C_Female.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
+
+#
+data2_F <- data2[data2$Gender == "Female", ]
+Res <- ForestFitXX(data2_F, "COPD", c("Sugar", "Depressed"), "permth_int",
+    Covs = c("Age"), PSM = c("Age"),
+    fullPlot = F, footnote = "Female. Adjust/Match age",
+    theme = NULL, Combine = T
+)
+#
+pdf("COPD_Sugar_DEP_adjust_PSM_C_T_Female.pdf", width = 8, height = 6)
+Res$Plot
+graphics.off()
+
+# ... Figure 4C, RCS ----
+reValues <- list("MCQ160O" = c("2" = "0", "7" = NA, "9" = NA))
+# NHrcs(Xitem = "Sugar", Yitem = "COPD", data = subDat, reValue = reValues)
+pdf("Sugar_RCS.pdf", width = 8, height = 6)
+NHrcs("Sugar", "COPD", subDat, nknots = 3, reValue = reValues)
+NHrcs("Sugar", "COPD", subDat, nknots = 3, reValue = reValues, facet = "Gender")
+dev.off()
 
 
 # === GWAS === ----
@@ -127,3 +309,106 @@ load("NH_Sugar.rda") # sugar
 load("bbj-a-103_P6_mr_res.rda")
 load("bbj-a-103_P6_mr_dat.rda")
 
+pdf("Sweets_DEP_bbj-a-103.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = T, xlim = c(0, 10)
+)
+graphics.off()
+
+## ... Figure S1, Table S5 ----
+dat$pos <- dat$pos.outcome
+dat$chr <- dat$chr.outcome
+dat_Gene <- chromSNP(dat, dat$SNP[1:3], Genes = T)
+sapply(dat$SNP[1:3], function(s) dat_Gene$transcripts[[s]][[1]][1]) %>% unlist()
+#
+head(dat)
+dat$outcome <- paste0("Chronic obstructive pulmonary disease || id:", dat$outcome)
+SelVars <- c(
+    "SNP", "exposure", "outcome", "effect_allele.exposure", "effect_allele.outcome",
+    "beta.exposure", "beta.outcome", "eaf.exposure", "eaf.outcome", "pval.exposure", "pval.outcome"
+)
+xlsx::write.xlsx(dat[, SelVars], "Table S5.xlsx")
+
+pdf("MR_scatter_plot_bbj-a-103.pdf", width = 8, height = 5)
+MR_plot(dat, res, rmID = F)
+graphics.off()
+
+## ... Figure 5B ----
+load("ebi-a-GCST90018587_P6_mr_res.rda")
+load("ebi-a-GCST90018587_P6_mr_dat.rda")
+
+pdf("Temp_ebi-a-GCST90018587.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = T, xlim = c(0, 10)
+)
+graphics.off()
+
+## ... Figure S1, Table S6 ----
+pdf("MR_scatter_plot_ebi-a-GCST90018587.pdf", width = 8, height = 5)
+MR_plot(dat, res, rmID = F)
+graphics.off()
+#
+head(dat)
+xlsx::write.xlsx(dat[, SelVars], "Table S6.xlsx")
+
+# Figure S2, Table S7 ----
+## ... Figure S2A ----
+load("ukb-b-20464_P6_mr_res.rda")
+
+pdf("Temp_ukb-b-20464.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = T
+)
+graphics.off()
+#
+load("ukb-b-20464_P6_mr_dat.rda")
+head(dat)
+xlsx::write.xlsx(dat[, SelVars], "Table S7.xlsx", sheetName = "ukb-b-20464")
+
+## ... Figure S2B ----
+load("ebi-a-GCST90018807_P6_mr_res.rda")
+
+pdf("Temp_ebi-a-GCST90018807.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = T
+)
+graphics.off()
+#
+load("ebi-a-GCST90018807_P6_mr_dat.rda")
+head(dat)
+xlsx::write.xlsx(dat[, SelVars], "Table S7.xlsx", sheetName = "ukb-b-GCST90018807", append = T)
+
+# Figure S3, Table S8 ----
+## ... Figure S3A ----
+# reverse_bbj-a-103
+load("reverse_bbj-a-103_mr_res.rda")
+
+pdf("reverse_bbj-a-103.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = F
+)
+graphics.off()
+#
+load("reverse_bbj-a-103_mr_dat.rda")
+head(dat)
+xlsx::write.xlsx(dat[, SelVars], "Table S8.xlsx", sheetName = "bbj-a-103")
+
+## ... Figure S3B ----
+# reverse_ebi-a-GCST90018587
+load("reverse_ebi-a-GCST90018587_mr_res.rda")
+
+pdf("reverse_ebi-a-GCST90018587.pdf", width = 10, height = 6)
+forest_mr(res,
+    append = T, exponentiate = T, wrap_str = 30,
+    rmID = F, combine = F
+)
+graphics.off()
+#
+load("reverse_ebi-a-GCST90018587_mr_dat.rda")
+head(dat)
+xlsx::write.xlsx(dat[, SelVars], "Table S8.xlsx", sheetName = "ebi-a-GCST90018587", append = T)
--- Functions.R
+++ Functions.R
@@ -40,3 +40,263 @@ trendGBD <- function(EPI, group = "sex", Measure = "Deaths", Metric = "Rate", Se
 }
 
 
+#' @title Predict a Bayesian age-period-cohort model
+#' @param EPI,GBD data.frames of disease counts and populations.
+#' @param nPredict number of years to predict.
+#' @param Measure `Deaths`, `Incidence`, `DALYs` (Disability-Adjusted Life Years), `YLDs` (Years Lived with Disability) or `YLLs` (Years of Life Lost).
+#' @param Region Region name of country or area.
+#' @param Sex `Female`, `Male`, or `Both`
+#' @param plot show plot.
+#' @param type `ageSpecProj` — projected age-specific counts, `ageStdRate` — projected age-standardized rates, `ageStdProj` — projected age-standardized counts
+#' @param ... Other parameters used in [plotBAPC].
+#'
+#' @importFrom reshape2 dcast
+#' @import BAPC
+#' @import dplyr
+#'
+#' @export
+#'
+PreBAPC <- function(EPI, GBD, nPredict = 10, Measure = "Deaths", Risk = NULL,
+                    Region = "Global", Sex = "Both", plot = T, type = "ageStdRate", ...) {
+    library(BAPC)
+    library(dplyr)
+    library(reshape2)
+    #
+    ageGrps <- c("<5", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90-94", "95+")
+    # build epi_mat
+    epi_dat <- if (!is.null(Risk)) filter(EPI, location == Region, sex == Sex, Measure == Measure, risk == Risk, metric == "Number") else filter(EPI, location == Region, sex == Sex, measure == Measure, metric == "Number")
+    epi_dat <- dcast(epi_dat, year ~ age, sum, value.var = "val") |> round()
+    rownames(epi_dat) <- epi_dat$year
+    epi_dat$year <- NULL
+    epi_dat <- epi_dat[, na.omit(match(ageGrps, colnames(epi_dat)))]
+    epi_dat <- epi_dat[, colSums(epi_dat, na.rm = T) != 0]
+    epi_dat <- epi_dat[rowSums(epi_dat, na.rm = T) != 0, ]
+
+    # project NAs for predict years
+    m <- as.numeric(max(rownames(epi_dat)))
+    predNA <- matrix(NA,
+        nrow = nPredict, ncol = ncol(epi_dat),
+        dimnames = list(seq(m + 1, m + nPredict, 1), colnames(epi_dat))
+    )
+    epi_dat <- rbind(epi_dat, predNA) |> data.frame(check.names = F)
+
+    # pyrs_dat
+    pyrs_dat <- filter(
+        GBD, location_name == Region, sex == Sex,
+        year_id %in% rownames(epi_dat),
+        age_group_name %in% colnames(epi_dat)
+    )
+
+    #
+    pyrs_dat <- dcast(pyrs_dat, year_id ~ age_group_name, sum, value.var = "val")
+    rownames(pyrs_dat) <- pyrs_dat$year_id
+    pyrs_dat$year_id <- NULL
+    pyrs_dat <- pyrs_dat[, na.omit(match(ageGrps, colnames(pyrs_dat)))]
+
+    # check dim
+    if (nrow(epi_dat) != nrow(pyrs_dat)) {
+        sub_years <- intersect(rownames(epi_dat), rownames(pyrs_dat))
+        pyrs_dat <- pyrs_dat[sub_years, ]
+        epi_dat <- epi_dat[sub_years, ]
+    }
+
+    # weights used to derive age-standardized projections
+    whostandard <- data.frame(
+        agegroup = ageGrps,
+        whoStandard = c(7.910, 9.568, 8.990, 8.324, 7.866, 7.633, 7.332, 6.811, 6.137, 5.509, 4.922, 4.346, 3.684, 2.991, 2.272, 1.607, 1.113, 0.617, 0.255, 0.085)
+    )
+
+    li_APC <- APCList(epi_dat, pyrs_dat, gf = 5)
+    result <- BAPC(li_APC,
+        predict = list(npredict = nPredict, retro = T),
+        secondDiff = FALSE,
+        stdweight = whostandard[match(colnames(epi_dat), whostandard$agegroup), 2]
+    )
+    # plot
+    if (plot) {
+        par(mfrow = c(1, 1))
+        plotBAPC(result, scale = 10^5, type = type, ...)
+        rates <- data.frame(result@agestd.rate)
+        rates$year <- as.numeric(rownames(rates))
+        lfit <- lm(rates$mean * 10^5 ~ seq_along(rates$year))
+        summ <- summary(lfit)
+        abline(coef = coef(lfit), lty = 1, col = "red", lwd = 2)
+        # lines(seq_along(rates$year), rates$mean * 10^5, type = "b", col = "red", lwd = 2, pch = 16)
+        fitPval <- summ$coefficients[2, "Pr(>|t|)"]
+        annot <- paste0("Coef = ", round(summ$coefficients[2, 1], 3), ", R^2 = ", round(summ$r.squared, 3), ", Pval", ifelse(fitPval < 0.001, " < 0.001", round(fitPval)))
+        title(paste0(Measure, " rate of gender:", Sex, ifelse(is.null(Risk), "", paste0(" due to ", Risk)), "\nfor populations of ", Region), cex.main = 1)
+        mtext(annot, 3, adj = 0, font = 4, col = "grey", cex = 0.8)
+        if (!is.null(Risk)) title(sub = paste("Risk:", Risk), col.sub = "grey", font.sub = 3)
+    }
+    return(result)
+}
+
+
+
+#' @title Forcasting Stochastic Mortality Model
+#' @param EPI,GBD data.frames of disease counts and populations.
+#' @param nPredict number of years to predict.
+#' @param Measure `Deaths`, `Incidence`, `DALYs` (Disability-Adjusted Life Years), `YLDs` (Years Lived with Disability) or `YLLs` (Years of Life Lost).
+#' @param Risk Risk factor depend on the `EPI` data type.
+#' @param Region Region name of country or area.
+#' @param Sex `Female`, `Male`, or `Both`
+#' @param model `LCA` is a standard Lee-Carter model by default, although many other options are available.
+#' @param StMM logical, add plot of simulate results of fitted model.
+#' @param periods a vector of three periods in `StMM`. Default is c('65', '75', '85').
+#' @param adjxy Adjust x,y postions of annotated text of `periods`.
+#' @param cols Colors of dotplot and fanplot of `periods`.
+#' @param ylims ylims of plot `StMM`.
+#' @param saveSim logical, save simulation results.
+#' @param ...
+#'
+#' @importFrom fanplot fan
+#' @importFrom reshape2 dcast
+#'
+#' @export
+#' @family APC functions
+#' @import demography
+#' @import StMoMo
+#'
+FSMM <- function(EPI, GBD, nPredict = 10, Measure = "Deaths", Risk = NULL, Region = "Global", Sex = "Both", model = "LCA", StMM = F, periods = c("65", "75", "85"), adjxy = c(0, 3), cols = NULL, ylims = NULL, saveSim = T, ...) {
+    library(demography, quietly = T)
+    library(StMoMo, quietly = T)
+    #
+    if (is.null(cols)) cols <- c("black", "green", "red", "blue", "cyan", "gold", "violet")
+    #
+    ageGrps <- c("<5", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90-94", "95+")
+    # build epi_mat
+    epi_dat <- if (!is.null(Risk)) filter(EPI, location == Region, sex == Sex, Measure == Measure, risk == Risk, metric == "Number") else filter(EPI, location == Region, sex == Sex, measure == Measure, metric == "Number")
+    epi_dat <- dcast(epi_dat, year ~ age, sum, value.var = "val") |> round()
+    rownames(epi_dat) <- epi_dat$year
+    epi_dat$year <- NULL
+    epi_dat <- epi_dat[, na.omit(match(ageGrps, colnames(epi_dat)))]
+    # NOTE zero is not allowed in epi_dat
+    epi_dat <- epi_dat[, colSums(epi_dat) != 0]
+
+    # pyrs_dat
+    pyrs_dat <- filter(
+        GBD, location_name == Region, sex == Sex,
+        year_id %in% rownames(epi_dat),
+        age_group_name %in% colnames(epi_dat)
+    )
+    #
+    pyrs_dat <- dcast(pyrs_dat, year_id ~ age_group_name, sum, value.var = "val")
+    rownames(pyrs_dat) <- pyrs_dat$year_id
+    pyrs_dat$year_id <- NULL
+    pyrs_dat <- pyrs_dat[, na.omit(match(ageGrps, colnames(pyrs_dat)))]
+
+    # check dim
+    if (nrow(epi_dat) != nrow(pyrs_dat)) {
+        # sub_years = setdiff(rownames(epi_dat), rownames(pyrs_dat))
+        sub_years <- intersect(rownames(epi_dat), rownames(pyrs_dat))
+        pyrs_dat <- pyrs_dat[sub_years, ]
+        epi_dat <- epi_dat[sub_years, ]
+    }
+    #
+    colnames(epi_dat) <- substring(colnames(epi_dat), 1, 2)
+    epi_dat <- rename(epi_dat, any_of(c("0" = "<5", "5" = "5-")))
+    # colnames(pyrs_dat) = substring(colnames(pyrs_dat), 1, 2)
+    ratio_dat <- t(epi_dat / pyrs_dat)
+    ages <- as.numeric(colnames(epi_dat))
+    years <- as.numeric(rownames(epi_dat))
+
+    # fit Lee-Carter model
+    demoDat <- demogdata(ratio_dat, t(pyrs_dat),
+        ages = ages, years = years,
+        type = "mortality",
+        label = Sex, name = Region
+    )
+    #
+    fit <- demography::lca(demoDat, adjust = "dt", minperiod = 5, interpolate = T)
+    par(mfrow = c(1, ifelse(StMM, 3, 2)))
+    plot(demoDat,
+        ylab = paste(Measure, " rate (log scale)"),
+        main = paste0(Sex, ": ", Region, " ", Measure, " rates \n(", paste0(range(fit$year), collapse = "-"), ")")
+    )
+    forfit <- forecast(fit, h = nPredict, jumpchoice = "actual")
+    plot(forfit,
+        ylab = paste(Measure, " rate (log scale)"),
+        main = paste0(Sex, ": ", Region, " ", Measure, " rates \n(", paste0(range(forfit$year), collapse = "-"), ")")
+    )
+    # save forecast result
+    if (saveSim) {
+        library(dplyr)
+        simFile <- paste0(Sex, "_", Region, "_", Measure, ".xlsx")
+        if (file.exists(simFile)) file.remove(simFile)
+        #
+        df_forfit <- expand.grid(Age = paste0(forfit$age, "~"), Year = forfit$year) %>%
+            mutate(
+                Rate = as.vector(forfit$rate[[1]]),
+                Lower = as.vector(forfit$rate[[2]]),
+                Upper = as.vector(forfit$rate[[3]])
+            )
+        #
+        xlsx::write.xlsx(df_forfit, simFile, sheetName = paste0(model, "_model"))
+    }
+
+    # add simulate plot
+    if (StMM) {
+        periods <- as.character(periods)
+        StDemoIniData <- StMoMoData(demoDat)
+        wxt <- genWeightMat(ages = ages, years = years, clip = 3)
+        LC <- lc(link = "logit") # Lee-Carter model
+        LCfit <- fit(LC, data = StDemoIniData, ages.fit = ages, wxt = wxt)
+        LCfor <- forecast(LCfit, h = nPredict)
+        set.seed(1234)
+        LCsim <- simulate(LCfit, nsim = 500, h = nPredict)
+        # save forecast result
+        if (saveSim) {
+            rates <- LCsim$rates
+            df_simu <- expand.grid(Age = paste0(dimnames(rates)[[1]], "~"), Year = dimnames(rates)[[2]], Sim = 1:dim(rates)[3]) %>%
+                mutate(Rate = as.vector(rates)) %>%
+                group_by(Age, Year) %>%
+                summarise(
+                    IQR = IQR(Rate, na.rm = T),
+                    Mean_Rate = mean(Rate, na.rm = T),
+                    SD_Rate = sd(Rate, na.rm = T)
+                )
+            xlsx::write.xlsx(data.frame(df_simu), simFile, sheetName = "Lee_Carter", append = T)
+        }
+
+        # fan plot
+        library(fanplot)
+        probs <- c(2.5, 10, 25, 50, 75, 90, 97.5)
+        qxt <- StDemoIniData$Dxt / StDemoIniData$Ext
+        # par(mar = c(4.5, 4, 1, 1))
+        xlims <- range(LCfit$years, LCsim$years)
+        if (is.null(ylims)) ylims <- range(qxt[periods, ], LCsim$rates[periods, , ])
+
+        # add fan plot of periods
+        for (i in seq_along(periods)) {
+            if (i == 1) {
+                plot(LCfit$years, qxt[periods[i], ],
+                    xlim = xlims, ylim = ylims,
+                    main = paste0(Sex, ": ", Region, " ", Measure, " rate\n of ages (", paste0(periods, collapse = ", "), ")"),
+                    xlab = "Year", ylab = paste0(Measure, " rate (log scale)"),
+                    pch = 20, log = "y", col = cols[i]
+                )
+                fan(t(LCsim$rates[periods[i], , ]),
+                    start = LCsim$years[1],
+                    probs = probs, n.fan = 4,
+                    fan.col = colorRampPalette(c(cols[i], "white")), ln = NULL
+                )
+                abline(v = LCsim$years[1], lty = 2)
+            } else {
+                points(LCfit$years, qxt[periods[i], ], pch = 20, col = cols[i])
+                fan(t(LCsim$rates[periods[i], , ]),
+                    start = LCsim$years[1],
+                    probs = probs, n.fan = 4,
+                    fan.col = colorRampPalette(c(cols[i], "white")), ln = NULL
+                )
+            }
+        }
+        # add labels
+        text(LCfit$years[1], qxt[periods, as.character(LCfit$years[1])],
+            labels = paste0("Age:", periods), adj = -adjxy, col = cols, font = 2
+        )
+    }
+    par(mfrow = c(1, 1))
+    if (!is.null(Risk)) title(sub = paste("Risk:", Risk), col.sub = "grey", font.sub = 3)
+}
+
+
